62173d5c321759578c78830452929f8c42227280
Raghavendra Gandanahalli <Raghavendra.Gandanahalli_EXT@MediaKind.com>
2024-01-25 19:13:35 +0530

    Login authentication implemented

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,6 +7,21 @@ const LoginPage = (props) => {
  
     const handleSubmit = (e) => {
         e.preventDefault();
+        const validUsers = [
+            { username: 'a@gmail.com', password: '123' },
+            { username: 'raghavendra@gmail.com', password: '456' },
+          ];
+   
+          const isValidUser = validUsers.some(user => user.username === email && user.password === pass);
+   
+          if (isValidUser) 
+          {
+             props.onFormSwitch('Routes');
+          } else {
+            alert('Invalid credentials. Please try again.');
+            setEmail('');
+            setPass('');
+          }
         console.log(email);
     }
  
@@ -24,7 +39,7 @@ const LoginPage = (props) => {
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
             <br></br>
             <br></br>
-            <button type="submit" onClick={() => props.onFormSwitch('Routes')}>Log In</button>
+            <button type="submit" >Log In</button>
         </form>
         {/* <button className="link-btn" onClick={() => props.onFormSwitch('homepage')}>Don't have an account? Register here.</button> */}
     </div>
