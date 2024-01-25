d7a4284ff8af7b50a2fc7e15caa0176e4727f5cc
Raghavendra Gandanahalli <Raghavendra.Gandanahalli_EXT@MediaKind.com>
2024-01-25 16:15:20 +0530

    Removed unwanted files

--- src/manageserver/servertable.js
+++ src/manageserver/servertable.js
@@ -1,21 +1,28 @@
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 // import { useEffect, useState } from 'react';
 import "C:/Mediaroom/src/Table.css";
 import React from "react";
 import servertable from "C:/Mediaroom/src/manageserver/data.json";
 
-const Servertable = ({Action = "Action"}) => {
-
-    const handleClick = (e) => {
-        (e.Status === "Running") ? console.log("Running") : alert('Your server restarted successfully!');
-};
+const Servertable = ({ Action = "Action" }) => {
+  const handleClick = (e) => {
+    e.Status === "Running"
+      ? console.log("Running")
+      : alert("Your server restarted successfully!");
+  };
 
   return (
     <div className="column">
-      
       {servertable ? (
         <table className="table">
-            <caption className="caption"><b><br></br><br></br>SERVER DETAILS</b><br></br><br></br></caption>
+          <caption className="caption">
+            <b>
+              <br></br>
+              <br></br>SERVER DETAILS
+            </b>
+            <br></br>
+            <br></br>
+          </caption>
           <thead>
             <tr>
               {Object.keys(servertable[0]).map((key, index) => (
@@ -31,7 +38,10 @@ const Servertable = ({Action = "Action"}) => {
                   <td key={cellIndex}>{cell}</td>
                 ))}
                 <td>
-<button onClick={() => handleClick(row)}>{row.Status === "Restart" ? "Running" : "Restart"}</button></td>
+                  <button onClick={() => handleClick(row)}>
+                    {row.Status === "Restart" ? "Running" : "Restart"}
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
