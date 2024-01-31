2f67a26decac1b94fb658d554b9e2069897babe0
Raghavendra Gandanahalli <Raghavendra.Gandanahalli_EXT@MediaKind.com>
2024-01-31 11:29:41 +0530

    Store user login data to google sheet

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,28 +1,56 @@
 import React, { useState } from "react";
 import "./login.css"
+import { useHistory } from "react-router-dom";
 
 const LoginPage = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
- 
-    const handleSubmit = (e) => {
+    let data = [];
+
+    const history = useHistory();
+       
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const validUsers = [
             { username: 'a@gmail.com', password: '123' },
             { username: 'raghavendra@gmail.com', password: '456' },
           ];
-   
-          const isValidUser = validUsers.some(user => user.username === email && user.password === pass);
-   
-          if (isValidUser) 
+          
+          const isValidUser = validUsers.some(user => user.username === email && user.password === pass);          if (isValidUser) 
           {
+            // User login data
+            data = [
+                {
+                    User: email,
+                    LoggedInDate: new Date().toString(),
+                }
+              ]
              props.onFormSwitch('Routes');
           } else {
             alert('Invalid credentials. Please try again.');
             setEmail('');
             setPass('');
           }
-        console.log(email);
+        // Store user login data to google sheet
+          try {
+            const res = await fetch(
+              "https://sheet.best/api/sheets/da498c34-41c5-452d-bb1f-a8dab2e5ea4aa",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+              }
+            );
+            if (res.ok) {
+              history.replace("/");
+            } 
+        } catch (error) {
+                console.log(error);
+              }
+        
+         
     }
  
     return (
@@ -32,7 +60,7 @@ const LoginPage = (props) => {
             <br></br>
             <br></br>
             {/* <label htmlFor="email">email</label> */}
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email"/>           
             {/* <label htmlFor="password">password</label> */}
             <br></br>
             <br></br>
