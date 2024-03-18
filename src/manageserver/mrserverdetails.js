8d98e4fa787a6210cfca5139c0b0a1a54a0f9838
Raghavendra Gandanahalli <Raghavendra.Gandanahalli_EXT@MediaKind.com>
2024-03-18 15:26:29 +0530

    UI changes

--- src/manageserver/mrserverdetails.js
+++ src/manageserver/mrserverdetails.js
@@ -1,21 +1,77 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import jsonData from "./mrserverdata.json";
-import "../Table.css";
+import "../grid.css";
 
-const ITEMS_PER_PAGE = 3;
+const ITEMS_PER_PAGE = 2;
+// const ROW_HEIGHT = 120;
 
-const Mrserverdetails = ({userData}) => {
+const Mrserverdetails = ({ userData }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchColumn, setSearchColumn] = useState('ComputerName');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchColumn, setSearchColumn] = useState("ComputerName");
+  // const [filteredData, setFilteredData] = useState(jsonData);
+  // const containerRef = useRef(null); //added now
+
+  //added now
+  // const scrollUp = () => {
+  //   if (containerRef.current) {
+  //     containerRef.current.scrollTop -= ROW_HEIGHT;
+  //   }
+  // };
+
+  // const scrollDown = () => {
+  //   if (containerRef.current) {
+  //     containerRef.current.scrollTop += ROW_HEIGHT;
+  //   }
+  // };
+  const handleStart = (index) => {
+    // Add logic to handle start action
+    alert("service started successfully");
+    console.log("Start action for index:", index);
+  };
+
+  const handleStop = (index) => {
+    // Add logic to handle stop action
+    alert("service stopped");
+    console.log("Stop action for index:", index);
+  };
+
+  const handleRestart = (index) => {
+    // Add logic to handle restart action
+    alert("service restarted");
+    console.log("Restart action for index:", index);
+  };
+
+
+  // useEffect(() => {
+  //   const totalPages = Math.ceil(filteredData.length / ITEMS_PER_PAGE);
+  
+  //   const handleKeyDown = (event) => {
+  //     if (event.key === "ArrowRight" && currentPage < totalPages) {
+  //       handleNextPage();
+  //     } else if (event.key === "ArrowLeft" && currentPage > 1) {
+  //       handlePreviousPage();
+  //     } else if (event.key === "ArrowUp") {
+  //       scrollUp();
+  //     } else if (event.key === "ArrowDown") {
+  //       scrollDown();
+  //     }
+  //   };
+  
+  //   document.addEventListener("keydown", handleKeyDown);
+  
+  //   return () => {
+  //     document.removeEventListener("keydown", handleKeyDown);
+  //   };
+  // }, [currentPage, filteredData,handleNextPage]); // Include handleNextPage in the dependency array
+  
+  // eslint-disable-next-line react-hooks/exhaustive-deps
 
   const handleSearchChange = (event) => {
     const value = event.target.value;
     setSearchTerm(value);
     setCurrentPage(1); // Reset current page when searching
-
-    // Optional: You can apply debounce to limit the frequency of filter calls
-    // debounceSearch(value);
+    // filterData(value);
   };
 
   const handleColumnChange = (event) => {
@@ -23,7 +79,6 @@ const Mrserverdetails = ({userData}) => {
     setSearchTerm(''); // Clear search term when column changes
     setCurrentPage(1);
   }; 
-
   const filteredData = jsonData.filter((item) => {
     const searchValue = item[searchColumn];
 
@@ -38,7 +93,6 @@ const Mrserverdetails = ({userData}) => {
       return searchValue === searchTerm;
     }
   });
-  
 
   const totalPages = Math.ceil(filteredData.length / ITEMS_PER_PAGE);
 
@@ -54,85 +108,130 @@ const Mrserverdetails = ({userData}) => {
   };
 
   return (
-    userData.role==="admin" ?
-    <div className="column">
-       <caption className="caption" style={{ marginTop: '180px'}}>
-            <b>
-              <br></br>
-              <br></br>PLANO SERVERS
-              <br></br>
-            </b>
-          </caption>
-          <br></br>
-      <div style={{ marginBottom: '10px', display: 'flex', alignItems: 'center'}}>
-              <div style={{height:'20px'}}>
-            {filteredData.length > 0 && (
-            <select value={searchColumn} onChange={handleColumnChange} style={{ marginRight: '140px' }}>
-          <option value="ComputerName">ComputerName</option>
-          <option value="Roles">Role</option>
-          <option value="ComputerStatus">ComputerStatus</option>
-          <option value="ServiceStatus">ServiceStatus</option>
-        </select>
-            )}
-            </div>
-            <input className="searchstyle"
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-        style={{ height: '20px', width: '200px', boxSizing: 'border-box' }}
-      />
-            </div>
-            {filteredData.length === 0 && (
-        <div style={{ textAlign: 'center', color: 'black' }}>No matching data found.</div>
-      ) }
+    <div style={{display:'flex',justifyContent:'center',alignContent:'center',flexDirection:'column',width:'1108px',height:'520px',marginBottom:'10px',overflow:'auto'}}>
+      <caption style={{fontSize:'30px',marginLeft:'-80px'}} >PLANO SERVERS</caption>
+      <div style={{ alignItems: 'center' }}>
+          {filteredData.length > 0 && (
+            <select
+              value={searchColumn}
+              onChange={handleColumnChange}
+              style={{marginLeft:'220px',width:'120px'}}
+            >
+              <option value="ComputerName">ComputerName</option>
+              {/* <option value="Roles">Role</option> */}
+              <option value="ComputerStatus">ComputerStatus</option>
+              <option value="ServiceStatus">ServiceStatus</option>
+            </select>
+          )}
+        <input
+          className="searchstyle"
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+          style={{ height: '20px', width: '200px', boxSizing: 'border-box',marginLeft:'350px'}}
+        />
+      </div>
+     <br></br>
       {filteredData.length > 0 && (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Computer Name</th>
-            <th>Roles</th>
-            <th>Computer Status</th>
-            <th>Service Status</th>
-          </tr>
-        </thead>
-        <tbody style={{ marginRight: '100px'}}>
+        //add ref={containerRef} in div for auto keys scroll
+        <div  style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', gridGap: '15px' ,overflowY:'inherit',overflowX: "hidden",}}>
           {filteredData.slice(startIndex, endIndex).map((item, index) => (
-            <tr key={index}>
-              <td>{item.ComputerName}</td>
-              <td>
+            <div key={index} className="grid-item">
+              <div>
+                <strong style={{color:'#154775'}}>Computer Name:</strong> {item.ComputerName}
+              </div>
+              <br></br>
+              <div>
+                <strong style={{color:'#154775'}}>Roles:</strong>
                 <ul>
                   {item.Roles.map((role, roleIndex) => (
                     <li key={roleIndex}>
-                      <strong>Name:</strong> {role.Name}, <strong>Type:</strong>{" "}
-                      {role.Type}
+                      <strong>Name:</strong> {role.Name},{" "}
+                      <strong>Type:</strong> {role.Type}
                     </li>
                   ))}
                 </ul>
-              </td>
-              <td>{item.ComputerStatus}</td>
-              <td>
+              </div>
+              <br></br>
+              <div>
+                <strong style={{color:'#154775'}} >Computer Status:</strong> {item.ComputerStatus}
+              </div>
+              <br></br>
+              <div>
+                <strong style={{color:'#154775'}}>Service Status:</strong>
                 <ul>
                   {item.ServiceStatus.map((role, roleIndex) => (
                     <li key={roleIndex}>
-                      <strong>Name:</strong> {role.Name}, <strong>Status:</strong>{" "}
-                      {role.Status}
+                      
+                      <strong>Name:</strong> {role.Name},{" "}
+                      <br></br>
+                      <strong>Status:</strong> <span style={{ color: role.Status === "Stopped" ? "red" : "green" ,marginRight:'150px'}}>{role.Status}</span>
+                      {role.Status === "Stopped" ? (
+      <button onClick={() => handleStart(index)} style={{marginRight: '10px', background:'#0cb061',padding:'2px'}}>Start</button>
+    ) : (
+      <>
+        <button onClick={() => handleStop(index)} style={{marginRight: '10px', background:'#b95d5d'}}>Stop</button>
+        <button onClick={() => handleRestart(index)} style={{background:'#635279'}}>Restart</button>
+      </>
+    )}
+                      {/* <span style={{ color: role.Status === "Stopped" ? "red" : "green" }}></span> */}
                     </li>
                   ))}
                 </ul>
-              </td>
-            </tr>
+              </div>
+              <br></br>
+              {/* <div>
+                <button onClick={() => handleStart(index)} style={{marginRight: '5px',background:'green',height:'2px'}}>Start</button>
+                <button onClick={() => handleStop(index)} style={{marginRight: '5px',background:'#b95d5d'}}>Stop</button>
+                <button onClick={() => handleRestart(index)} style={{background:'#154775'}}>Restart</button>
+              </div> */}
+            </div>
           ))}
-        </tbody>
-      </table>
+        </div>
       )}
-       <div style={{ textAlign: 'center', marginTop: '10px',marginRight:'150px' ,marginBottom:'10px'}}>
-        <button onClick={handlePreviousPage} disabled={currentPage === 1 || filteredData.length === 0} style={{ color: currentPage === 1 || filteredData.length === 0 ? 'gray' : 'black', cursor: currentPage === 1 || filteredData.length === 0 ? 'default' : 'pointer' }}>{'<'}</button>
-        <span style={{ margin: '0 10px' }}>Page {currentPage} of {totalPages}</span>
-        <button onClick={handleNextPage} disabled={currentPage === totalPages || filteredData.length === 0} style={{ color: currentPage === totalPages || filteredData.length === 0 ? 'gray' : 'black' , cursor: currentPage === totalPages || filteredData.length === 0 ? 'default' : 'pointer'  }}>{'>'}</button>
+      <br></br>
+      <div
+       style={{ textAlign: 'center'}}
+      >
+        <button
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1 || filteredData.length === 0}
+          style={{
+            color:
+              currentPage === 1 || filteredData.length === 0
+                ? "gray"
+                : "black",
+            cursor:
+              currentPage === 1 || filteredData.length === 0
+                ? "default"
+                : "pointer",
+          }}
+        >
+          {"<"}
+        </button>
+        <span style={{ margin: "0 10px" }}>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages || filteredData.length === 0}
+          style={{
+            color:
+              currentPage === totalPages || filteredData.length === 0
+                ? "gray"
+                : "black",
+            cursor:
+              currentPage === totalPages || filteredData.length === 0
+                ? "default"
+                : "pointer",
+          }}
+        >
+          {">"}
+        </button>
       </div>
+      
     </div>
-    :<div>You are not Authorised</div>
   );
 };
 
